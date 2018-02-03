8f4e5db4b1a3c3fd00cb80795e9ac5ee789de414
mikespriggs26 <mikespriggs@msn.com>
2018-02-02 19:56:43 -0500

    Project completed

--- BankAccount-python/BankAccount_python.py
+++ BankAccount-python/BankAccount_python.py
@@ -28,27 +28,35 @@ class CheckingAccount():
         print("Your checking account balance is $" + str(self.checking_balance))
     def make_checking_deposit(self):
         print("Your beginning balance is $" + str(self.checking_balance))
-        self.checking_balance += self.deposit_amount
-        print("You have deposited $" + str(self.deposit_amount))
+        self.checking_balance += self.checking_deposit_amount
+        print("You deposited $" + str(self.checking_deposit_amount))
         print("Your new checking account balance is $" + str(self.checking_balance))
     def make_checking_withdrawal(self):
-        if checking_account.withdrawal_amount > self.checking_balance:
-            self.checking_balance -= self.withdrawal_amount
-            print("You have withdrawn $" + str(self.withdrawal_amount))
-            print("Your new checking account balance is $" + str(self.withdrawal_balance))
+        if self.checking_withdrawal_amount < self.checking_balance:
+            self.checking_balance -= self.checking_withdrawal_amount
+            print("You withdrew $" + str(self.checking_withdrawal_amount))
+            print("Your new checking account balance is $" + str(self.checking_balance))
         else:
             print("You do not have sufficient funds.  Please try again.")
-
-
-
-
 
 class SavingsAccount():
     def __init__(self, savings_balance):
         self.savings_balance = savings_balance
     def account_balance(self):
         print("Your savings account balance is $" + str(self.savings_balance))
-
+    def make_savings_deposit(self):
+        print("Your beginning balance is $" + str(self.savings_balance))
+        self.savings_balance += self.savings_deposit_amount
+        print("You deposited $" + str(self.savings_deposit_amount))
+        print("Your new savings account balance is $" + str(self.savings_balance))
+    def make_savings_withdrawal(self):
+        if self.savings_withdrawal_amount < self.savings_balance:
+            print("Your beginning balance is $" + str(self.savings_balance))
+            self.savings_balance -= self.savings_withdrawal_amount
+            print("You withdrew $" + str(self.savings_withdrawal_amount))
+            print("Your new savings account balance is $" + str(self.savings_balance))
+        else:
+            print("You do not have sufficient funds. Please try again.")
 
 
 
@@ -68,22 +76,51 @@ def main():
     options_list.append("3. Make a Deposit")
     options_list.append("4. Make a Withdrawal")
     options_list.append("5. Exit")
-    for option in options_list:
-        print(option)
-    user_option = input("Please select a number from the list above and press enter:")
-    if user_option == "1":
-        customer.accountInfo()
-    elif user_option == "2":
-        account_info_input = input("View which account balance?  1. Checking  2. Savings")
-        if account_info_input == "1":
-            checking_account.checking_balance()
-        elif account_info_input == "2":
-            savings_account.savings_balance()
-    elif user_option =="3":
-        account_deposit_input = input("Into which account?  1. Checking  2. Savings")
-        if account_deposti_input == "1":
-            checking_account.deposit_amount = int(input("Enter deposit amount:"))
-            checking_account.make_checking_deposit
+    user_option = ""
+
+    while user_option != 5:
+        for option in options_list:
+            print(option)
+        user_option = input("Please select a number from the list above and press enter:     ")
+    
+        if user_option == "1":
+            customer.accountInfo()
+            print()
+            
+        elif user_option == "2":
+            account_info_input = input("View which account balance?  1. Checking  2. Savings     ")
+            if account_info_input == "1":
+                checking_account.account_balance()
+                print()
+            elif account_info_input == "2":
+                savings_account.account_balance()
+                print()
+
+        elif user_option == "3":
+            account_deposit_input = input("Into which account?  1. Checking  2. Savings     ")
+            if account_deposit_input == "1":    
+                checking_account.checking_deposit_amount = int(input("Enter deposit amount with no dollar sign:    "))
+                checking_account.make_checking_deposit()
+                print()
+            elif account_deposit_input == "2":
+                savings_account.savings_deposit_amount = int(input("Enter withdrawal amount with no dollar sign:    "))
+                savings_account.make_savings_deposit()
+                print()
+        
+        elif user_option == "4":
+            account_withdrawal_input =  input("From which account?  1. Checking  2. Savings     ")
+            if account_withdrawal_input =="1":
+                checking_account.checking_withdrawal_amount = int(input("Enter withdrawal amount with no dollar sign:    "))
+                checking_account.make_checking_withdrawal()
+            elif account_withdrawal_input =="2":
+                savings_account.savings_withdrawal_amount = int(input("Enter withdrawal amount with no dollar sign:    "))
+                savings_account.make_savings_withdrawal()
+
+        elif user_option == "5":
+            raise SystemExit
+    
+
+
     #while user_option != 5:
         
             
